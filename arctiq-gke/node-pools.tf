190e57b31ac2954ca1128cd5d8eac013b8a05d72
Masrur Hossain <mdmasrur3426@gmail.com>
2022-10-21 17:49:27 -0400

    added oauth for terraform

--- arctiq-gke/node-pools.tf
+++ arctiq-gke/node-pools.tf
@@ -23,7 +23,6 @@ resource "google_container_node_pool" "general" {
     service_account = google_service_account.kubernetes.email
     oauth_scopes = [
         "https://www.googleapis.com/auth/cloud-platform",
-        "https://www.googleapis.com/auth/devstorage.read_only"
     ]
   }
 }
@@ -58,7 +57,10 @@ resource "google_container_node_pool" "spot" {
 
     service_account = google_service_account.kubernetes.email
     oauth_scopes = [
-        "https://www.googleapis.com/auth/cloud-platform"
+        "https://www.googleapis.com/auth/cloud-platform",
+        "https://www.googleapis.com/auth/devstorage.read_only",
+        "https://www.googleapis.com/auth/servicecontrol",
+        "https://www.googleapis.com/auth/service.management.readonly"
     ]
   }
 }

8dd5283a4d52a2833cee2eb577db43f257408175
Masrur Hossain <mdmasrur3426@gmail.com>
2022-10-22 21:56:06 -0400

    fixed port issue

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from models import models
 from flask import Flask, request, render_template, redirect
 
@@ -90,4 +91,4 @@ def edit(id):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))

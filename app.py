6b110c7c753e9583eac63960029b187c9756bd9f
Masrur Hossain <mdmasrur3426@gmail.com>
2022-10-22 22:51:39 -0400

    fix

--- app.py
+++ app.py
@@ -5,12 +5,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-
 @app.route('/')
 def hello_world():
     target = os.environ.get('TARGET', 'World')
     return 'Hello {}!\n'.format(target)
-
 
 
 if __name__ == "__main__":
